de23b4a8f6f6b1119caafdbe5017521c3180e85e
aldenluthfi <aldenluthfi@gmail.com>
2025-02-20 18:32:53 +0700

    edit: file structure

--- bin/main/src/main.rs
+++ bin/main/src/main.rs
@@ -2,10 +2,10 @@ use io;
 use game::board::Board;
 
 fn main() {
-    let mut board: Board = Board::new(10, 12);
+    let mut board: Board = Board::new(8, 8);
 
     board.set_bit(0, 0);
-    board.set_bit(0, 1);
+    board.set_bit(7, 7);
 
     io::print_board(&board);
 }
