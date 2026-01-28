a52e374dbb88b5724e0cd169abc83350c6e636c2
Alden Luthfi <alden.arrahman@tiket.com>
2026-01-29 01:21:29 +0700

    bug: forgot to rev stack

--- src/main/src/main.rs
+++ src/main/src/main.rs
@@ -1,22 +1,33 @@
 use game::{
-    moves::move_parse::{
-        parse_move,
-        compound_atomic_to_vector,
-        chained_atomic_to_vector,
-    },
-    representations::state::State,
+    constants::I, moves::move_parse::{
+        chained_atomic_to_vector, cleanup_atomic_vectors, compound_atomic_to_vector, parse_move
+    }, representations::{board::Board, state::State}
 };
 
-use io::game_io::parse_config_file;
+use io::{board_io::format_board, game_io::parse_config_file};
 
 fn main() {
     State::init_global(parse_config_file("configs/test.anm"));
+    let state = State::global();
 
-    // let expr: String = parse_move(&"n<<nNnW>nF>");
-    let expr: String = parse_move(&"<FnF>nW");
+    let expr: String = parse_move(&"<n<N>.nW>");
 
     println!("Expr: {}", expr);
-    for vec in compound_atomic_to_vector(&expr, "n") {
-        println!("{:?}", vec);
+    let raw_vecs = compound_atomic_to_vector(&expr, "n");
+    println!("Raw Vectors: {:?}", raw_vecs);
+    let vecs = cleanup_atomic_vectors(raw_vecs);
+    println!("Cleaned Vectors: {:?}", vecs);
+
+    let mut board = Board::new(state.files, state.ranks);
+
+    board.set_bit(I, 7);
+
+    for (f, r) in vecs {
+        println!("Setting bit at file {}, rank {}", f, r);
+        board.set_bit((I as i8 + f) as u8, (7 + r) as u8);
     }
+
+    println!("{}", format_board(&board, Some('X')));
+
+
 }
