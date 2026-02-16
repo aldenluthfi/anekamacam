76a2bf7434715085d5c611a2e211c5fbd148b402
Alden Luthfi <aldenluthfi@gmail.com>
2026-02-16 18:19:01 +0700

    add: xiangqi 2

--- src/game/moves/perft.rs
+++ src/game/moves/perft.rs
@@ -1,11 +1,10 @@
 use std::fs;
 
 use crate::{
-    game::{
-        moves::move_list::{generate_all_moves, make_move, undo_move},
+    constants::{MULTI_CAPTURE_MOVE, SINGLE_CAPTURE_MOVE}, game::{
+        moves::move_list::{generate_all_moves, is_in_check, make_move, undo_move},
         representations::state::State
-    },
-    io::move_io::format_move
+    }, io::{game_io::format_game_state, move_io::format_move}, move_type
 };
 
 fn parse_perft_file(
@@ -65,6 +64,7 @@ pub fn start_perft(
         perft_cases.iter().enumerate()
     {
         state.load_fen(fen);
+        println!("{}", format_game_state(state, false));
 
         let expected_perfts = [
             perft_1, perft_2, perft_3, perft_4, perft_5, perft_6,
@@ -76,24 +76,28 @@ pub fn start_perft(
             let elapsed = start_time.elapsed().as_nanos();
 
             let expected = expected_perfts[(d - 1) as usize];
-
-            if result == *expected {
+            if result.0 == *expected {
                 successful_cases += 1;
-                total_moves += result;
+                total_moves += result.0;
                 println!(
                     "{:04}. FEN: {:<width$} | Depth: {} | Expected: {:>12} | \
-                    Result: {:>12} | Time: {:>12} [PASSED]",
-                    i, fen, d, expected, result, format_time(elapsed),
+                    Result: {:>12} | Time      : {:>12} [PASSED]",
+                    i, fen, d, expected, result.0, format_time(elapsed),
                     width = longest_fen
                 );
             } else {
                 println!(
                     "{:04}. FEN: {:<width$} | Depth: {} | Expected: {:>12} | \
-                    Result: {:>12} | Time: {:>12} [FAILED]",
-                    i, fen, d, expected, result, format_time(elapsed),
+                    Result: {:>12} | Time      : {:>12} [FAILED]",
+                    i, fen, d, expected, result.0, format_time(elapsed),
                     width = longest_fen
                 );
             }
+            println!(
+                "           {:<width$}              \
+                Captures: {:>12} | Checks: {:>12} | Checkmates: {:>9}",
+                "", result.1, result.2, result.3, width = longest_fen
+            );
         }
     }
 
@@ -110,54 +114,73 @@ fn perft(
     depth: u8,
     debug: bool,
     branch: Option<u8>,
-) -> u64 {
+) -> (u64, u64, u64, u64) {
     perft_impl(state, depth, debug, branch, String::new())
 }
 
+// returns the tuple of (nodes, captures, checks, checkmates)
 fn perft_impl(
     state: &mut State,
     depth: u8,
     debug: bool,
     branch: Option<u8>,
     prefix: String,
-) -> u64 {
+) -> (u64, u64, u64, u64) {
+
     if depth == 0 {
         if debug {
             println!("{} Reached leaf node", prefix);
         }
-        return 1;
+
+        let is_check = is_in_check(state.playing, state);
+        return (1, 0, is_check as u64, 0);
     }
 
     let possible_moves = generate_all_moves(state);
     let mut nodes = 0;
+    let mut captures = 0;
+    let mut checks = 0;
+    let mut checkmates = 0;
 
     for mv in possible_moves {
+        let move_type = move_type!(mv);
+
+        let mut is_capture = false;
+        if depth == 1 {
+            is_capture =
+                move_type == SINGLE_CAPTURE_MOVE ||
+                move_type == MULTI_CAPTURE_MOVE;
+        }
+
         if debug {
             let formatted_move = &format_move(&mv, state);
 
             if make_move(state, mv) {
-                nodes += perft_impl(
+                let (n, c, ch, cm) = perft_impl(
                     state,
                     depth - 1,
                     debug && depth > branch.unwrap_or(0),
                     branch,
-                    format!(
-                        "{} {}",
-                        prefix,
-                        formatted_move
-                    ),
+                    format!("{} {}", prefix, formatted_move),
                 );
+                nodes += n;
+                captures += c + is_capture as u64;
+                checks += ch;
+                checkmates += cm;
                 undo_move(state);
             }
         } else if make_move(state, mv) {
-            nodes += perft_impl(
+            let (n, c, ch, cm) = perft_impl(
                 state,
                 depth - 1,
                 false,
                 None,
                 String::new(),
             );
-
+            nodes += n;
+            captures += c + is_capture as u64;
+            checks += ch;
+            checkmates += cm;
             undo_move(state);
         }
     }
@@ -166,5 +189,9 @@ fn perft_impl(
         println!("{} moves | Nodes: {}", prefix, nodes);
     }
 
-    nodes
+    if depth == 1 && nodes == 0 && is_in_check(state.playing, state){
+        checkmates += 1;
+    }
+
+    (nodes, captures, checks, checkmates)
 }
